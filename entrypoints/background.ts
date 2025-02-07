0bfe2427b56068ecad797d6ae45663d24c6d70b4
Karl Dreher <karldreher@outlook.com>
2025-02-06 19:24:16 -0800

    feat: wip

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,6 @@
 export default defineBackground(() => {
   console.log('Hello background!', { id: browser.runtime.id });
+  // This should work in getting the storage to be gett-able from content script but doesnt
+  chrome.storage.session.setAccessLevel({ accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS' });
+
 });
--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,42 @@
+async function redditHandler(ignore: string) {
+  let d = document.getElementsByTagName("article");
+
+  Array.from(d).forEach(element => {
+      let ariaLabel = element.getAttribute('aria-label');
+      if (ariaLabel && ariaLabel.toLowerCase().includes(ignore.toLowerCase())) {
+          element.remove();
+          console.log('removed', ariaLabel);
+      }
+  });
+}
+
+async function getOptions(): Promise<string[]> {
+  try {
+    const ignores = await storage.getItem('local:ignores');
+    console.log(ignores);
+    if (Array.isArray(ignores)) {
+      return ignores;
+    }
+  } catch {
+    return [];
+  }
+  return [];
+}
+
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
-  },
+  matches: ['*://*.google.com/*', '*://*.reddit.com/*'],
+  world: 'MAIN',
+  async main() {
+    console.log("content script loaded");
+    const observer = new MutationObserver(async () => {
+      const options = await getOptions();
+      console.log(options)
+      // TODO: make sure each handler only called on the specific origin
+      options.forEach(ignore => {
+      redditHandler(ignore);
+      });
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
 });
